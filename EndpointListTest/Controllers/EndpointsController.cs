7d0d6256ed5ef3a8e7778b32af08f67de4eb8f0d
Martin Fylke <martin.fylke@volvocars.com>
2021-04-15 14:30:34 +0200

    IP

--- EndpointListTest/Controllers/EndpointsController.cs
+++ EndpointListTest/Controllers/EndpointsController.cs
@@ -26,47 +26,15 @@ namespace EndpointListTest.Controllers
         [HttpGet]
         public async Task<IEnumerable<string>> Get()
         {
+            var inClusterConfig = KubernetesClientConfiguration.InClusterConfig();
+            var client = new Kubernetes(inClusterConfig);
 
-
-            Kubernetes client = null;
-            try
-            {
-                var config = KubernetesClientConfiguration.BuildConfigFromConfigFile();
-                client = new Kubernetes(config);
-            }
-            catch (Exception e)
-            {
-                _logger.Log(LogLevel.Error, e, "Failed to create client");
-            }
-
-            try
-            {
-                var specifiedFileConfig = KubernetesClientConfiguration.BuildConfigFromConfigFile(Environment.GetEnvironmentVariable("KUBECONFIG"));
-                if (client == null)  client = new Kubernetes(specifiedFileConfig);
-            }
-            catch (Exception e)
-            {
-                _logger.Log(LogLevel.Error, e, "Failed to create specified file client");
-            }
-
-            try
-            {
-                var inClusterConfig = KubernetesClientConfiguration.InClusterConfig();
-                if (client == null) client = new Kubernetes(inClusterConfig);
-            }
-            catch (Exception e)
-            {
-                _logger.Log(LogLevel.Error, e, "Failed to create in cluster client");
-            }
-
-            if (client == null) throw new Exception("Unable to create any type of client");
-            
             var pods = await client.ListNamespacedPodAsync("gpe");
 
             var jsonList = new List<string>();
             foreach (var pod in pods.Items)
             {
-                jsonList.Add(JsonSerializer.Serialize(pod));
+                jsonList.Add(JsonSerializer.Serialize(pod.Status.HostIP));
             }
 
             return jsonList;
